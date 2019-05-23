58e07e00609686be50de4662df500282220b946a
TylerBlood <tylerlblood@gmail.com>
2019-05-23 14:08:20 -0400

    starting branch

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
 <?php 
 
-    // multi-dimensional arrays
 
-    $blogs = [
-        ['title'=>'mario party', 'author'=>'mario', 'content'=>'lorem', 'likes'=>30],
-        ['title'=>'mario kart cheats', 'author'=>'toad', 'content'=>'lorem', 'likes'=>25],
-        ['title'=>'zelda hidden chests', 'author'=>'link', 'content'=>'lorem', 'likes'=>50],
-    ];
-
-    // print_r($blogs[1][1]);
-    // echo $blogs[2]['author'];
-    // echo count($blogs);
-
-    $blogs[] = ['title' => 'castle party', 'author'=>'peach', 'content'=>'lorem', 'likes'=>100];
-    // print_r($blogs);
-
-    $popped = array_pop($blogs);
-    print_r($popped);
 
 ?>
 
