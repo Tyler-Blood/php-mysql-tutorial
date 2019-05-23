fe2e843b5a4e4bf655c3ef3cb1166b612446f589
TylerBlood <tylerlblood@gmail.com>
2019-05-23 15:43:57 -0400

    completed lesson 13

--- index.php
+++ index.php
@@ -1,40 +1,21 @@
 <?php 
 
-// conditional statements
+    // functions
 
-$price = 20;
-
-// if ($price < 10) {
-//     echo 'the condition is met';
-// } elseif ($price < 30) {
-//     echo 'elseif condition met';
-// } else {
-//     echo 'condition not met';
-// }
-
-$products = [
-		['name' => 'shiny star', 'price' => 20],
-		['name' => 'green shell', 'price' => 10],
-		['name' => 'red shell', 'price' => 15],
-		['name' => 'gold coin', 'price' => 5],
-		['name' => 'lightning bolt', 'price' => 40],
-		['name' => 'banana skin', 'price' => 2]
-];
-
-foreach($products as $product){
-
-    if($product['name'] === 'lightning bolt'){
-        break;
+    function sayHello($name = 'shaun', $time = 'morning'){
+        echo "good $time $name";
     }
 
-    if($product['price'] > 15){
-        continue;
+    // sayHello();
+    sayHello('yoshi', 'night');
+
+    function formatProduct($product){
+        // echo "{$product['name']} costs £{$product['price']} to buy <br />";
+        return "{$product['name']} costs £{$product['price']} to buy <br />";
     }
 
-    echo $product['name'] . '<br/>';
-
-}
-
+    // $formatted = formatProduct(['name' => 'gold star', 'price' => 20]);
+    // echo $formatted;
 
 ?>
 
