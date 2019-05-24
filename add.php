224662e52d699b2295d849cd8f918d7974c8a0a3
TylerBlood <tylerlblood@gmail.com>
2019-05-23 23:31:55 -0400

    completed lesson 20

--- add.php
+++ add.php
@@ -1,21 +1,35 @@
 <?php 
-
     if(isset($_POST['submit'])){
+        $email = $title = $ingredients = '';
+        
         // check email
         if(empty($_POST['email'])){
             echo 'An email is required <br/>';
-        } else {
-            echo htmlspecialchars($_POST['email']);
+        } else{
+            $email = $_POST['email'];
+            if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                echo 'Email must be a valid email address';
+            }
         }
+
+        // check title
         if(empty($_POST['title'])){
             echo 'A title is required <br/>';
-        } else {
-            echo htmlspecialchars($_POST['title']);
+        } else{
+            $title = $_POST['title'];
+            if(!preg_match('/^[a-zA-Z\s]+$/', $title)){
+                echo 'Title must be letters and spaces only';
+            }
         }
+
+        // check ingredients
         if(empty($_POST['ingredients'])){
             echo 'An ingredient is required <br/>';
-        } else {
-            echo htmlspecialchars($_POST['ingredients']);
+        } else{
+            $ingredients = $_POST['ingredients'];
+            if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)){
+                echo 'Ingredients must be a comma separated list';
+            }
         }
     } // end of post check
 
@@ -31,12 +45,12 @@
         <form class="white" action="add.php" method="POST">
             <label for="email">Your Email:</label>
             <input type="text" name="email">
-            <label for="email">Pizza Title:</label>
+            <label>Pizza Title:</label>
             <input type="text" name="title">
-            <label for="email">Ingredients (comma seperated):</label>
+            <label>Ingredients (comma separated):</label>
             <input type="text" name="ingredients">
             <div class="center">
-                <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
+                <input type="submit" name="submit" value="Submit" class="btn brand z-depth-0">
             </div>
         </form>
     </section>
